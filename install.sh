c805b03244b1badf9d46e754e253b2220a889225
Guilherme Salgado <gsalgado@gmail.com>
2015-03-13 11:38:03 +0000

    Tweak to my needs
     * Install bash-completion and other golang tools
     * Set CDPATH
     * Write all golang env vars to /opt/goenv, which is picked up by my .bashrc

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 apt-get update
 apt-get install -y --no-install-recommends \
+    openssh-server \
+    vim-gtk \
     git \
     mercurial \
     tig \
-    emacs \
-    xclip \
-    ack-grep \
+    bash-completion \
+    language-pack-en \
     python3
 
 apt-get autoremove
 
+go get github.com/axw/gocov/gocov
+go get golang.org/x/tools/cmd/gorename
+go get github.com/rogpeppe/godef
+go get github.com/nsf/gocode
 go get golang.org/x/tools/cmd/goimports
-go get code.google.com/p/rog-go/exp/cmd/godef
+go get golang.org/x/tools/cmd/oracle
+go get github.com/golang/lint/golint
+go get github.com/kisielk/errcheck
+go get github.com/jstemmer/gotags
 go get -u github.com/davecgh/go-spew/spew
