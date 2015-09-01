6b6b21e19a2a7bbe7a9132bac0d310b4c9edfb4f
Guilherme Salgado <gsalgado@gmail.com>
2015-09-01 08:34:17 +0100

    Drop a bunch of things I used to install

--- install.sh
+++ install.sh
@@ -5,22 +5,10 @@ echo "deb-src http://ppa.launchpad.net/ubuntu-elisp/ppa/ubuntu trusty main" >> /
 apt-get update
 apt-get install -y --no-install-recommends \
     openssh-server \
-    vim-gtk \
-    git \
-    mercurial \
-    tig \
     bash-completion \
     language-pack-en \
-    python3 \
     tmux
 
 apt-get autoremove
 
-go get golang.org/x/tools/cmd/gorename
-go get github.com/rogpeppe/godef
-go get github.com/nsf/gocode
-go get golang.org/x/tools/cmd/goimports
-go get golang.org/x/tools/cmd/oracle
-go get github.com/golang/lint/golint
-go get github.com/jstemmer/gotags
 go get -u github.com/davecgh/go-spew/spew
